b2a0aa4d00aeb17bf038bab1aa7d093afa476c51
JakeOh <jake.oh.lecture@gmail.com>
2020-11-27 09:14:53 +0900

    Add files via upload

---
+++ lab_oracle/sql04.sql
@@ -0,0 +1,9 @@
+/*
+Oracle 연습계정 hr 활성화
+1. 명령 프롬프트를 실행
+2. sqlplus를 사용해서 SYSTEM/oracle(계정/비밀번호) sysdba 권한으로 접속
+3. SQL 명령 프롬프트 상태에서 hr계정의 비밀번호를 hr로 설정하면서 unlock
+4. hr/hr(계정/비밀번호) 접속 테스트
+5. 명령 프롬프트 종료
+6. SQL Developer에서 hr 계정으로 접속할 수 있도록 설정.
+*/
