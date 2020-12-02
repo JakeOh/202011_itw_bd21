22d3c6b88ff6cfda64b8644fe9f91e1f2868d595
JakeOh <jake.oh.lecture@gmail.com>
2020-12-02 15:30:43 +0900

    Add files via upload

--- lab_oracle/sql09.sql
+++ lab_oracle/sql09.sql
@@ -13,6 +13,39 @@ from 테이블1 join종류 테이블2 on join조건
 
 -- emp, dept 테이블에서 사번, 이름, 부서번호, 부서이름을 검색
 -- 1) Oracle 문법
-select emp.empno, emp.ename, emp.deptno, dept.dname
-from emp, dept
-where emp.deptno = dept.deptno;
+select e.empno, e.ename, e.deptno, d.dname
+from emp e, dept d
+where e.deptno = d.deptno;
+
+-- 2) ANSI 표준 문법
+select e.empno, e.ename, e.deptno, d.dname
+from emp e inner join dept d
+    on e.deptno = d.deptno;
+-- inner join에서 inner는 생략 가능.
+
+-- left (outer) join
+-- emp, dept 테이블에서 사번, 이름, 부서번호, 부서이름 검색
+-- emp 테이블에 있는 모든 부서 번호가 검색 결과로 나오도록 검색
+-- 1) Oracle 방식
+select e.empno, e.ename, e.deptno, d.dname
+from emp e, dept d
+where e.deptno = d.deptno(+);
+
+-- 2) ANIS 표준 문법
+select e.empno, e.ename, e.deptno, d.dname
+from emp e left outer join dept d
+    on e.deptno = d.deptno;
+-- left outer join에서 outer는 생략 가능.
+
+-- right (outer) join
+-- emp, dept 테이블에서 사번, 이름, 부서번호, 부서이름을 검색
+-- deptno 테이블의 부서번호와 부서이름은 모두 출력
+-- 1) Oracle 문법
+select e.empno, e.ename, d.deptno, d.dname
+from emp e, dept d
+where e.deptno(+) = d.deptno;
+
+-- 2) ANSI 표준 문법
+select e.empno, e.ename, d.deptno, d.dname
+from emp e right outer join dept d
+    on e.deptno = d.deptno;
