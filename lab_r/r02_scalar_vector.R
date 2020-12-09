8647c78d89c747b9d2eaee14798011d625f0491e
JakeOh <jake.oh.lecture@gmail.com>
2020-12-09 15:22:22 +0900

    Add files via upload

--- lab_r/r02_scalar_vector.R
+++ lab_r/r02_scalar_vector.R
@@ -36,3 +36,29 @@ numbers[2:4]  # 2 <= index <= 4 범위의 원소 선택
 # 3) 특정 위치(인덱스) 여러곳의 원소들을 선택:
 numbers[c(1, 4, 6)]
 
+# R에서 변수에 값을 저장(할당)할 때: 변수 <- 값
+# 변수는 Global Environment에 생기게 됨.
+# 함수를 호출할 때 함수에게 argument를 전달할 때: arg = 값
+
+# 함수(function): 기능. 연산.
+# argument: 함수를 호출할 때 함수에게 전달하는 값.
+# 필수(mandatory) argument: 함수를 호출할 때 반드시 전달해야 하는 값.
+# 선택(optional) argument: 기본값(default)이 설정되어 있어서, 
+# 함수를 호출할 때 생략해도 되는 값.
+# parameter: argument를 저장하기 위한 함수 내부의 변수.
+# return value(반환 값): 함수가 기능을 수행한 후 반환하는 값. 함수 수행 결과.
+
+# seq(): Sequence.
+# 함수를 호출할 때, 파라미터 이름을 생략하고 argument를 전달함.
+evens <- seq(2, 10, 2)  # 2부터 10까지 2씩 증가하는 숫자들로 이루어진 vector.
+
+# 함수를 호출할 때, 어떤 파라미터에 무슨 값을 전달할 지를 지정함.
+odds <- seq(from = 1, to = 10, by = 2) # 1부터 10까지 2씩 증가하는 숫자들로 이루어진 vector.
+
+# optional argument들을 전달하지 않으면(생략하면), 기본값이 사용됨.
+numbers <- seq(from = 1, to = 10)  # by의 기본값 1이 사용됨.
+numbers
+
+numbers <- seq(to = 5)  # from=1, by=1 기본값이 사용됨.
+numbers
+
