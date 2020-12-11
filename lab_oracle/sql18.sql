6b8dc667c241ab7d971c5d05d33e8c0951df3f1b
JakeOh <jake.oh.lecture@gmail.com>
2020-12-11 17:34:33 +0900

    Add files via upload

--- lab_oracle/sql18.sql
+++ lab_oracle/sql18.sql
@@ -25,3 +25,209 @@
 18) 15번 문제의 결과에서 대륙이름이 컬럼이 되도록 출력하세요.
 */
 
+-- 2)
+create table gapminder (
+    country     varchar2(100),
+    continent   varchar2(100),
+    year        number(4),
+    life_exp    number,
+    pop         number,
+    gdp_percap  number
+);
+
+desc gapminder;
+
+-- 3)
+select * from gapminder;
+
+-- 4)
+select count(distinct country) from gapminder;
+
+-- 5)
+select count(distinct continent) from gapminder;
+
+-- 6)
+select min(year), max(year) from gapminder;
+select distinct year from gapminder order by year;
+
+-- 7)
+select * from gapminder
+where life_exp = (
+    select max(life_exp) from gapminder
+    );
+    
+-- 8)
+select * from gapminder
+where pop = (
+    select max(pop) from gapminder
+    );
+    
+-- 9)
+select * from gapminder
+where gdp_percap = (
+    select max(gdp_percap) from gapminder
+    );
+    
+-- 10)
+select distinct country from gapminder 
+where lower(country) like '%korea%';
+
+select * from gapminder
+where lower(country) = 'korea, rep.';
+
+-- 11)
+select * from gapminder
+where (year, gdp_percap) in (
+    select year, max(gdp_percap) from gapminder
+    group by year
+    )
+order by year;
+
+-- 12)
+select * from gapminder
+where (continent, gdp_percap) in (
+    select continent, max(gdp_percap) from gapminder
+    group by continent
+    )
+order by continent;
+
+-- 13)
+select year, continent, sum(pop) as population 
+from gapminder
+group by year, continent
+order by year, continent;
+
+select year, continent, sum(pop) as population
+from gapminder
+group by year, continent
+order by population desc;
+
+with a as (
+    select year, continent, sum(pop) as population
+    from gapminder
+    group by year, continent
+)
+select year, continent from a
+where population = (
+    select max(population) from a
+);
+
+
+-- 14)
+select year, continent, round(avg(life_exp), 1) as avg_life_exp
+from gapminder
+group by year, continent
+order by year, continent;
+
+select year, continent, round(avg(life_exp), 1) as avg_life_exp
+from gapminder
+group by year, continent
+order by avg_life_exp desc;
+
+with a as (
+    select year, continent, round(avg(life_exp), 1) as avg_life_exp
+    from gapminder
+    group by year, continent
+)
+select year, continent from a
+where avg_life_exp = (
+    select max(avg_life_exp) from a
+);
+
+-- 15)
+select year, continent, round(avg(gdp_percap), 1) as avg_gdp_percap
+from gapminder
+group by year, continent
+order by year, continent;
+
+select year, continent, round(avg(gdp_percap), 1) as avg_gdp_percap
+from gapminder
+group by year, continent
+order by avg_gdp_percap desc;
+
+with a as (
+    select year, continent, round(avg(gdp_percap), 1) as avg_gdp_percap
+    from gapminder
+    group by year, continent
+)
+select year, continent from a
+where avg_gdp_percap = (
+    select max(avg_gdp_percap) from a
+);
+
+-- 16)
+with subqr as (
+    select year, continent, pop from gapminder
+)
+select * from subqr
+pivot (
+    sum(pop) for continent in ('Africa' as africa,
+        'Americas' as america,
+        'Asia' as asia,
+        'Europe' as europe,
+        'Oceania' as oceania
+    )
+)
+order by year;
+
+-- 17)
+with subqr as (
+    select year, continent, life_exp from gapminder
+)
+select * from subqr
+pivot (
+    avg(life_exp) for continent in ('Africa' as africa,
+        'Americas' as america,
+        'Asia' as asia,
+        'Europe' as europe,
+        'Oceania' as oceania
+    )
+)
+order by year;
+
+with subqr as (
+    select year, continent, round(avg(life_exp), 1) as avg_life_exp
+    from gapminder
+    group by year, continent
+)
+select * from subqr
+pivot (
+    max(avg_life_exp) for continent in ('Africa' as africa,
+        'Americas' as america,
+        'Asia' as asia,
+        'Europe' as europe,
+        'Oceania' as oceania
+    )
+)
+order by year;
+
+-- 18)
+with subqr as (
+    select year, continent, gdp_percap from gapminder
+)
+select * from subqr
+pivot (
+    avg(gdp_percap) for continent in ('Africa' as africa,
+        'Americas' as america,
+        'Asia' as asia,
+        'Europe' as europe,
+        'Oceania' as oceania
+    )
+)
+order by year;
+
+with subqr as (
+    select year, continent, round(avg(gdp_percap), 1) as avg_gdp_percap
+    from gapminder
+    group by year, continent
+)
+select * from subqr
+pivot (
+    max(avg_gdp_percap) for continent in ('Africa' as Africa,
+        'Americas' as America,
+        'Asia' as Asia,
+        'Europe' as Europe,
+        'Oceania' as Oceania
+    )
+)
+order by year;
