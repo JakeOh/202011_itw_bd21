2065d0764392c0c62caca065336c1ef84e24988a
JakeOh <jake.oh.lecture@gmail.com>
2020-12-16 10:23:43 +0900

    Add files via upload

--- lab_r/r13_dplyr.R
+++ lab_r/r13_dplyr.R
@@ -2,7 +2,56 @@
 # poptotal(인구수), popadults(성인 인구수), popasian(아시아계 인구수)
 # 1) '인구 대비 미성년 인구 백분율' 파생 변수를 추가.
 # 2) 미성년 인구 비율이 높은 상위 5개 county와 백분율을 출력.
-# 3) 미성년 인구 비율 등급 파생 변수를 추가.
+# 3) 미성년 인구 비율 등급 파생 변수를 추가. 각 등급의 빈도수.
 #    40% 이상 large, 30 ~ 40% 미만 middle,  30% 미만 small
 # 4) '인구 대비 아시아계 인구 백분율' 파생 변수를 추가하고, 상위 10개 지역의
 # state, county, 아시아계 인구 비율을 출력.
+#> midwest에는 이미 percasian 변수가 있습니다. 그 값과 비교해보세요.
+
+# tidyverse 패키지를 검색 경로에 추가(메모리에 로딩)
+library(tidyverse)
+search()
+
+# ggplot2::midwest 데이터 셋 확인.
+# str(midwest)
+str(ggplot2::midwest)
+head(midwest)
+
+# 1) 미성년 인구 비율(백분율)
+midwest_df <- midwest %>% 
+  mutate(child_pct = (poptotal - popadults) / poptotal * 100)
+# (1 - popadults / poptotal) * 100
+
+select(midwest_df, county, poptotal, popadults, child_pct)
+
+# 2) 미성년 인구 비율 상위 5개
+midwest_df %>% 
+  arrange(desc(child_pct)) %>%  # 미성년 인구 비율 내림차순 정렬
+  head(n = 5) %>%               # 상위 5개 row 선택
+  select(county, child_pct)     # 변수(column) 선택
+
+# 3) 미성년 인구 비율 등급
+midwest_df <- midwest_df %>% 
+  mutate(child_grade = ifelse(child_pct >= 40, 'large',
+                              ifelse(child_pct >= 30, 'middle', 'small')))
+midwest_df %>% select(county, child_pct, child_grade)
+
+# child_grade 빈도수(frequency table)
+table(midwest_df$child_grade)
+
+midwest_df %>% 
+  group_by(child_grade) %>% 
+  summarize(n = n())
+
+midwest_df %>% count(child_grade)
+
+# count() 함수: 
+# group_by() %>% summarize(n = n())를 간단히 하는 도우미 함수.
+
+# 4) 아시아계 인구 비율 상위 10개 지역
+midwest_df %>% 
+  mutate(asia_pct = (popasian / poptotal) * 100) %>% 
+  arrange(desc(asia_pct)) %>% 
+  head(n = 10) %>% 
+  select(state, county, asia_pct)
+
