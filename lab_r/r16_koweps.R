be54c29a98bd080134f6d908e60111eafc915643
JakeOh <jake.oh.lecture@gmail.com>
2020-12-18 14:25:55 +0900

    Add files via upload

--- lab_r/r16_koweps.R
+++ lab_r/r16_koweps.R
@@ -24,3 +24,58 @@ tail(df_job)
 
 welfare <- left_join(welfare, df_job, by = c('job_code' = 'code_job'))
 head(welfare)
+
+# 가장 많은 인구가 종사하는 직종 상위 10개의 이름, 인구수
+# NA 개수 확인
+welfare %>% filter(is.na(job_code)) %>% summarize(n = n())
+welfare %>% filter(is.na(job)) %>% summarize(n = n())
+
+job_top10 <- welfare %>% 
+  filter(!is.na(job)) %>%  # job이 있는 자료들만 선택
+  group_by(job) %>%        # job별 그룹
+  summarize(n = n()) %>%   # 그룹별 개수로 요약
+  arrange(desc(n)) %>%     # 개수의 내림차순 정렬
+  head(n = 10)             # 상위 10개
+
+job_top10
+
+job_top10 <- welfare %>% 
+  filter(!is.na(job)) %>% 
+  count(job) %>%  # count(var_name): group_by(var_name) %>% summarize(n = n())
+  arrange(desc(n)) %>% 
+  head(n = 10)
+
+job_top10
+
+ggplot(data = job_top10) +
+  geom_col(mapping = aes(x = job, y = n))
+
+ggplot(data = job_top10) +
+  geom_col(mapping = aes(x = n, y = reorder(job, n))) +
+  ylab('job')
+
+# 남성이 가장 많이 일하는 직종 상위 10개, 인구수. 시각화
+job_male_top10 <- welfare %>% 
+  filter(!is.na(job) & gender == 'Male') %>%  # 직업이 있는 남성 선택
+  count(job) %>% 
+  arrange(desc(n)) %>% 
+  head(10)
+
+job_male_top10
+
+ggplot(data = job_male_top10) +
+  geom_col(mapping = aes(x = n, y = reorder(job, n))) +
+  ylab('job')
+
+# 여성이 가장 많이 일하는 직종 상위 10개, 인구수. 시각화
+job_female_top10 <- welfare %>% 
+  filter(!is.na(job) & gender == 'Female') %>% 
+  count(job) %>% 
+  arrange(desc(n)) %>% 
+  head(10)
+
+job_female_top10
+
+ggplot(data = job_female_top10) +
+  geom_col(mapping = aes(x = n, y = reorder(job, n))) +
+  ylab('job')
