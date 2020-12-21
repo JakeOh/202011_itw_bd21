701102ea1be5171c23f9036fa11e0d2d8f9a4695
JakeOh <jake.oh.lecture@gmail.com>
2020-12-21 10:44:09 +0900

    Add files via upload

--- lab_r/r17_koweps.R
+++ lab_r/r17_koweps.R
@@ -6,15 +6,25 @@ head(welfare)
 
 # 지역별 인구 분석
 # 지역코드(region_code), 지역이름(region)을 가지고 있는 데이터 프레임
-region_list <- data.frame(region_code =1:7,
-                          region =c('서울',
-                                    '수도권(인천/경기)',
-                                    '부산/경남/울산',
-                                    '대구/경북',
-                                    '대전/충남',
-                                    '강원/충북',
-                                    '광주/전남/전북/제주도'))
+region_list <- data.frame(region_code = 1:7,
+                          region = c('서울',
+                                     '수도권(인천/경기)',
+                                     '부산/경남/울산',
+                                     '대구/경북',
+                                     '대전/충남',
+                                     '강원/충북',
+                                     '광주/전남/전북/제주도'))
 region_list
 
 # welfare 데이터 프레임과 region_list 데이터 프레임을 join
+welfare <- left_join(welfare, region_list)
+head(welfare)
+
+# 지역별 인구수 - 테이블, 그래프
+
+# 지역별 성별 인구수 - 테이블, 그래프.
+
+
+
+
 
