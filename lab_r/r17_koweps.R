6abbd27c30339f485f1b1941345e06ec48757765
JakeOh <jake.oh.lecture@gmail.com>
2020-12-21 12:01:14 +0900

    Add files via upload

--- lab_r/r17_koweps.R
+++ lab_r/r17_koweps.R
@@ -21,10 +21,64 @@ welfare <- left_join(welfare, region_list)
 head(welfare)
 
 # 지역별 인구수 - 테이블, 그래프
+table(welfare$region)
+sum(table(welfare$region))
+
+ggplot(data = welfare) +
+  geom_bar(mapping = aes(y = region))
 
 # 지역별 성별 인구수 - 테이블, 그래프.
+welfare %>% count(region, gender)
+welfare %>% group_by(region, gender) %>% summarise(n = n())
 
+ggplot(data = welfare) +
+  geom_bar(mapping = aes(x = region, fill = gender))  # position = 'stack'
 
+ggplot(data = welfare) +
+  geom_bar(mapping = aes(x = region, fill =  gender),
+           position = 'dodge')
 
+# 지역별 성비(gender ratio)
+ggplot(data = welfare) +
+  geom_bar(mapping = aes(y = region, fill =  gender),
+           position = 'fill')
+
+# welfare 데이터 프레임에 ages 변수를 추가
+#   age < 30이면 'young', age < 60이면 'middle', age >= 60이면, 'old'
+# welfare$new_var <- ...
+welfare <- welfare %>% 
+  mutate(ages = ifelse(age < 30, 'young',
+                       ifelse(age < 60, 'middle', 'old')))
+table(welfare$ages)
+
+# ages 변수를 factor로 변환(카테고리 타입 변수로 변환) - as.factor(), factor()
+welfare$ages <- factor(welfare$ages,
+                       levels = c('young', 'middle', 'old'))
+table(welfare$ages)
+
+# region별, ages별 인구수 - 테이블, 그래프
+df <- welfare %>% count(region, ages)
+df
+
+ggplot(data = welfare) +
+  geom_bar(mapping = aes(x = region, fill = ages))
+
+ggplot(data = welfare) +
+  geom_bar(mapping = aes(x = region, fill = ages),
+           position = 'dodge')
+
+ggplot(data = welfare) +
+  geom_bar(mapping = aes(x = region, fill = ages),
+           position = 'fill')
+
+ggplot(data = welfare) +
+  geom_bar(mapping = aes(y = region, fill = ages),
+           position = 'fill')
+
+# region별 income의 평균을 찾고, 시각화.
+
+# region별 gender별 income의 평균을 찾고, 시각화.
+
+# region별 ages별 income의 평균을 찾고, 시각화.
 
 
