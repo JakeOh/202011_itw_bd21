5236f7dab3e8df7b47bc92df053ae8f48949f657
JakeOh <jake.oh.lecture@gmail.com>
2020-12-22 17:37:38 +0900

    Add files via upload

---
+++ lab_r/r21_covid_map.R
@@ -0,0 +1,13 @@
+# https://github.com/nytimes/covid-19-data
+#   미국의 Covid-19(코로나 바이러스) 데이터가 업데이트되는 github
+# https://www.nytimes.com/interactive/2020/us/coronavirus-us-cases.html
+#   위의 github의 데이터로 시각화한 그래프들이 있는 NY Times 사이트.
+# github의 설명을 잘 읽어보고, 
+# 미국의 주별 코로나 확진자 수를 지도 위에 시각화하기 위해 필요한 데이터를 찾아서
+# 미국 지도 위에 현재 확진자 수를 시각화해보세요.
+
+# NY Times의 여러 시각화 그래프들을 보시고,
+# 같은 그래프들을 만들어 보세요.
+
+# 대한민국의 시도별 코로나 바이러스 확진자수 데이터를 찾을 수 있으면,
+# 대한민국의 지도 위에 확진자수를 시각화해보세요.
