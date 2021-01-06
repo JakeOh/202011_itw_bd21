828be068907a89992e64067de37bea124956bd1b
JakeOh <jake.oh.lecture@gmail.com>
2021-01-06 16:34:08 +0900

    Add files via upload

--- lab_r/ml07_linear_regression.R
+++ lab_r/ml07_linear_regression.R
@@ -78,3 +78,63 @@ cov(insurance$expenses, insurance$bmi) / (sd(insurance$expenses) * sd(insurance$
 
 cor(insurance$expenses, insurance$age)
 cor(insurance$expenses, insurance$bmi)
+
+pairs.panels(insurance)
+
+# 3. 모델 훈련 -----
+# 다항 선형 회귀 모델 생성, 훈련
+
+# 데이터를 훈련 셋/테스트 셋으로 분리
+train_size <- round(1338 * 0.8)
+
+train_set <- insurance[1:train_size, ]
+test_set <- insurance[(train_size + 1):1338, ]
+
+summary(train_set$expenses)
+summary(test_set$expenses)
+
+# 선형 회귀 모델을 훈련 셋으로 학습시킴(fitting to linear model).
+# formula = expenses ~ .
+# expenses ~ age + sex + bmi + children + smoker + region
+lin_reg1 <- lm(formula = expenses ~ ., data = train_set)
+lin_reg1
+# expenses = b0 + b1 * age + b2 * sexmale + b3 * bmi + b4 * children +
+#            b5 * sm_yes + b6 * r_nw + b7 * r_se + b8 * r_sw
+# 카테고리 타입의 변수들은 one-hot encoding돼서 선형회귀식에 포함됨.
+
+# 선형회귀식의 계수들(절편과 각 변수의 기울기들)
+lin_reg1$coefficients
+
+# 모델이 찾은 선형회귀식을 사용해서 계산한 훈련 셋의 각 샘플들의 (의료비) 예측값.
+lin_reg1$fitted.values[1:5]
+
+# 훈련 셋에서 (의료비) 실제값
+train_set$expenses[1:5]
+
+# residual: 오차 = 실제값 - 예측값
+train_set$expenses[1:5] - lin_reg1$fitted.values[1:5]
+lin_reg1$residuals[1:5]
+
+# residual들의 기술 통계량 요약
+summary(lin_reg1$residuals)
+summary(train_set$expenses)
+
+# 훈련 셋에서의 평가 지표: MSE, RMSE, MAE
+mse(actual = train_set$expenses, predicted = lin_reg1$fitted.values)
+rmse(actual = train_set$expenses, predicted = lin_reg1$fitted.values)  #> 6030.798
+sqrt(mean(lin_reg1$residuals ^ 2))
+
+mae(actual = train_set$expenses, predicted = lin_reg1$fitted.values)  #> 4151.354
+mean(abs(lin_reg1$residuals))
+
+# lm 모델에서는 mse와 rmse를 간단히 계산할 수 있음.
+mse(modelObject = lin_reg1)
+rmse(modelObject = lin_reg1)
+# lm 모델에서 mae()는 모델 객체로 계산할 수 없음!
+
+# lm 모델 객체의 summary
+summary(lin_reg1)
+
+
+
+
