aa062612504061ab16869de35c1bd70929d18446
JakeOh <jake.oh.lecture@gmail.com>
2021-01-07 17:34:10 +0900

    Add files via upload

--- lab_r/ml09_regression.R
+++ lab_r/ml09_regression.R
@@ -81,9 +81,33 @@ summary(lin_reg2)
 #> Residual standard error: 0.6505
 #> Multiple R-squared:  0.3443,	Adjusted R-squared:  0.3386
 
-# 3.
+# 테스트 셋 평가
+test_predicts2 <- predict(object = lin_reg2, newdata = X_test2)
+test_residuals2 <- X_test2$quality - test_predicts2
+summary(test_residuals2)
+rmse(X_test2$quality, test_predicts2)  #> 0.641315
+mae(X_test2$quality, test_predicts2)  #> 0.5065519
+
+# 3. Regression Tree -----
 # Regression Tree 모델을 학습(scaling되지 않은 변수들을 사용)
 # 훈련 셋/테스트 셋의 RMSE, MAE 계산해서 Linear Regression의 결과들과 비교
 
+reg_tree <- rpart(formula = quality ~ ., data = X_train)
+reg_tree
+rpart.plot(reg_tree)
 
+# 훈련 셋 예측
+train_predicts3 <- predict(object = reg_tree, newdata = X_train)
+# 훈련 셋 residuals 요약
+summary(X_train$quality - train_predicts3)
+# 훈련 셋 RMSE, MAE
+rmse(X_train$quality, train_predicts3)  #> 0.6245433
+mae(X_train$quality, train_predicts3)  #> 0.5066823
 
+# 테스트 셋 예측
+test_predicts3 <- predict(object = reg_tree, newdata = X_test)
+# 테스트 셋 residuals 요약
+summary(X_test$quality - test_predicts3)
+# 테스트 셋 RMSE, MAE
+rmse(X_test$quality, test_predicts3)  #> 0.6603269
+mae(X_test$quality, test_predicts3)  #> 0.5405416
