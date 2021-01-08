47ef6126f5ebde20567cd74ed0954bd43d620e68
JakeOh <jake.oh.lecture@gmail.com>
2021-01-08 17:23:14 +0900

    Add files via upload

--- lab_r/ml12_svr.R
+++ lab_r/ml12_svr.R
@@ -31,3 +31,35 @@ summary(X_test$strength)
 # SVM 예측기(regressor)를 훈련 셋으로 학습시킴
 svm_reg <- ksvm(x = strength ~ ., data = X_train, kernel = 'vanilladot')
 svm_reg
+
+# 훈련 셋 평가
+train_pred <- predict(object = svm_reg, newdata = X_train)
+train_pred[1:10]
+X_train$strength[1:10]
+rmse(actual = X_train$strength, predicted = train_pred)  #> 11.8534
+
+# 4. 테스트 셋으로 모델 평가 -----
+test_pred <- predict(object = svm_reg, newdata = X_test)
+rmse(actual = X_test$strength, predicted = test_pred)
+
+# 5. kernel 변경에 따른 효과 -----
+# 1) Gaussian RBF 커널:
+svr_rbf <- ksvm(x = strength ~ ., data = X_train, kernel = 'rbfdot')
+svr_rbf
+rbf_pred <- predict(object = svr_rbf, newdata = X_test)
+summary(X_test$strength - rbf_pred)  # residuals 분포
+rmse(actual = X_test$strength, predicted = rbf_pred)  #> 6.911415
+
+# 2) Polynomial 커널:
+svr_poly <- ksvm(x = strength ~ ., data = X_train,
+                 kernel = 'polydot',       # kernel = 커널 종류
+                 kpar = list(degree = 3))  # kpar = 커널 파라미터(kernel parameter)
+svr_poly
+poly_pred <- predict(object = svr_poly, newdata = X_test)
+summary(X_test$strength - poly_pred)  # residuals 분포
+rmse(actual = X_test$strength, predicted = poly_pred)  #> 6.503291
+
+
+# 6. SVM 이외의 ML 알고리즘과 비교 -----
+# lm(): Linear Regression, rpart(): Regression Tree
+
