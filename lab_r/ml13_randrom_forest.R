3bfd220d4df4b679fad30e60ef4207bd1e9add34
JakeOh <jake.oh.lecture@gmail.com>
2021-01-11 12:17:41 +0900

    Add files via upload

--- lab_r/ml13_randrom_forest.R
+++ lab_r/ml13_randrom_forest.R
@@ -32,4 +32,26 @@ table(X_test$Species)
 
 
 # 3. Random Forest 알고리즘을 학습 -----
+forest_clf <- randomForest(formula = Species ~ ., data = X_train)
+forest_clf
+#> OOB estimate of  error rate: 5.83%
 
+# 훈련 셋 평가
+train_pred <- predict(object = forest_clf, newdata = X_train)
+mean(X_train$Species == train_pred)  #> 100%
+CrossTable(x = X_train$Species, y = train_pred, prop.chisq = FALSE)
+# oob estimate의 결과와 훈련 셋의 예측 결과는 다르다!
+# oob estimate의 결과는 테스트 셋의 예측 결과와 비슷!
+
+# 테스트 셋 평가
+test_pred <- predict(object = forest_clf, newdata = X_test)
+mean(X_test$Species == test_pred)  #> 0.9333333
+CrossTable(x = X_test$Species, y = test_pred, prop.chisq = FALSE)
+
+# OOB(Out Of Bagging)
+#   Bagging(Bootstrap Aggregating)에서 한번도 샘플링되지 않는 훈련 셋의 관찰값들
+# OOB Estimate: OOB 샘플들을 사용해서 모델을 테스트하는 방법.
+
+# 4. Random Forest 분류 연습
+# wisc_bc_data.csv 데이터(Benign/Malignant)
+# credit.csv 데이터(default/not default)
